bd7ed305a5750419e1144ce0f22ec36bd55d4b5d
nazarpunk <nazarpunk@gmail.com>
2021-03-19 15:45:37 +0200

    SharedWorker

--- index.ts
+++ index.ts
@@ -1,70 +1,72 @@
-class ActiveTab {
-	static is_init: boolean = false;
-	static key: string = `active-tab`;
-	static id: number = performance.now();
-	static is_current: boolean = true;
+const localstorage_key = `tab-ts`;
 
-	static tabs: {
-		[key: number]: {
-			id: 0,
-			ts: 0,
-			visible: boolean
-		}
-	} = {};
+let id = '';
+const a = "qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM_-";
+for (let i = 0; i < 32; i++) id += a[Math.floor(Math.random() * a.length)];
 
-	static on_update() {
-		let last: {
-			id: number,
-			ts: number
-		} = {
-			id: 0,
-			ts: 0
-		};
-		for (const [, v] of Object.entries(this.tabs)) {
-			if (v.ts > last.ts) last = v;
-		}
-		this.is_current = last.id === this.id;
-		document.title = this.is_current ? `---` : `0`;
+
+if (!!window.SharedWorker) {
+	//const worker = new SharedWorker(`sharedworker.js`);
+
+	//worker.port.postMessage([first.value, second.value]);
+	//console.log('Message posted to worker');
+
+	/*
+	worker.port.onmessage = function(e) {
+		console.log('Message received from worker');
+		console.log(e.lastEventId);
 	}
 
-	static on_visibility_change() {
-		const obj: {
-			id: number,
-			ts: number,
-			visible: boolean
-		} = {
-			id     : this.id,
-			ts     : performance.now(),
-			visible: document.visibilityState === `visible`
-		};
-		localStorage.setItem(this.key, JSON.stringify(obj));
-		// @ts-ignore
-		this.tabs[this.id] = obj;
-		this.on_update();
-	}
-
-	static init() {
-		if (this.is_init) return;
-		this.is_init = true;
-		window.addEventListener(`storage`, e => {
-			if (e.key !== this.key) return;
-			const obj = JSON.parse(e.newValue || `{}`);
-			if (obj.hasOwnProperty(`remove`)) delete this.tabs[obj.remove];
-			else this.tabs[obj.id] = obj;
-			this.on_update();
-		});
-
-		window.addEventListener(`beforeunload`, () => {
-			localStorage.setItem(this.key, JSON.stringify({remove: this.id}));
-		});
-		document.addEventListener('visibilitychange', () => this.on_visibility_change());
-		document.addEventListener("DOMContentLoaded", () => this.on_visibility_change());
-
-	}
+	 */
 }
 
-ActiveTab.init();
+interface tab_data {
+	id: string
+	ts: number
+	visible: boolean
+}
 
-console.log(`check`);
+const tabs: {[key: string]: tab_data} = {};
 
-export {};
+let is_current: boolean = false;
+
+const update = () => {
+	let last: tab_data | null = null;
+	for (const [, v] of Object.entries(tabs)) {
+		if (!last || v.ts > last.ts) last = v;
+	}
+	is_current = last ? last.id === id : false;
+	document.title = is_current ? `+` : `-`;
+}
+
+const event = () => {
+	const obj: tab_data = {
+		id     : id,
+		ts     : Date.now(),
+		visible: document.visibilityState === `visible`
+	};
+	localStorage.setItem(localstorage_key, JSON.stringify(obj));
+	tabs[id] = obj;
+	update();
+}
+
+window.addEventListener(`storage`, e => {
+	if (e.key !== localstorage_key) return;
+	const obj = JSON.parse(e.newValue || `{}`);
+	if (obj.hasOwnProperty(`remove`)) delete tabs[obj.remove];
+	else tabs[obj.id] = obj;
+	update();
+});
+
+document.addEventListener(`DOMContentLoaded`, event);
+document.addEventListener(`visibilitychange`, event);
+window.addEventListener(`beforeunload`, () => {
+	localStorage.setItem(localstorage_key, JSON.stringify({remove: id}));
+});
+
+// noinspection JSUnusedGlobalSymbols
+const is_tab_active = (): boolean => {
+	return true;
+}
+
+export {is_tab_active};
